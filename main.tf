9304775ef2219fc226b1179b3168f828873e3359
Ismael <ismael_1_55@hotmail.com>
2025-11-07 11:42:34 +0100

    Fix docker container image reference to use .name instead of .latest

--- main.tf
+++ main.tf
@@ -17,7 +17,8 @@ resource "docker_image" "nginx" {
 # Crear un contenedor a partir de la imagen
 resource "docker_container" "web" {
   name  = "web_server"
-  image = docker_image.nginx.latest
+  # 💡 CORRECCIÓN: Usar .name en lugar de .latest
+  image = docker_image.nginx.name 
   ports {
     internal = 80
     external = 8080
